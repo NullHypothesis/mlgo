d61a18d26d7237f825606bcbb8d0e4369e6ec922
David Jh Shih <djh.shih@gmail.com>
2012-02-25 14:25:51 -0500

    Added functions to test equality of floating points.

---
+++ cluster/equal.go
@@ -0,0 +1,44 @@
+package cluster
+
+import "math"
+
+func ApproximatelyEqual(a, b, epsilon float64) bool {
+	var t float64
+	if math.Fabs(a) < math.Fabs(b) {
+		t = math.Fabs(b)
+	} else {
+		t = math.Fabs(a) * epsilon
+	}
+	return math.Fabs(a - b) <= t
+}
+
+func EssentiallyEqual(a, b, epsilon float64) bool {
+	var t float64
+	if math.Fabs(a) > math.Fabs(b) {
+		t = math.Fabs(b)
+	} else {
+		t = math.Fabs(a) * epsilon
+	}
+	return math.Fabs(a - b) <= t
+}
+
+func DefinitelyGreaterThan(a, b, epsilon float64) bool {
+	var t float64
+	if math.Fabs(a) < math.Fabs(b) {
+		t = math.Fabs(b)
+	} else {
+		t = math.Fabs(a) * epsilon
+	}
+	return (a - b) > t
+}
+
+func DefinitelyLessThan(a, b, epsilon float64) bool {
+	var t float64
+	if math.Fabs(a) < math.Fabs(b) {
+		t = math.Fabs(b)
+	} else {
+		t = math.Fabs(a) * epsilon
+	}
+	return (a - b) < t
+}
+
